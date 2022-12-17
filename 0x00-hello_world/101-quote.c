2c6207adb0d995249d3f693adfd001cadfeebf3a
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-17 03:37:39 +0100

    quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	char[70] quote;
+	int i
+
+	quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	i = 0;
+	while (quote[i] != "\0")
+	{
+		putchar(quote[i];
+		i++;
+	}
+
+	return (1);
+}
