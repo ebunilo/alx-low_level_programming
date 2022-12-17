b6bc8779415a51711bca2c494ec72651fa5dea27
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-17 02:06:21 +0100

    use printf()

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * return 0
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
