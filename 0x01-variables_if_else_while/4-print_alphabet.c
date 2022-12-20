21ead007bdd18b63d327baae30a5a51932d3c087
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-21 00:28:33 +0100

    print lowercase letters with some exceptions

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point into the program
+ * @void: Does not accept any parameter
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c = 'z'; c++)
+	{
+		if (c != 'e' or c != 'q')
+			putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
