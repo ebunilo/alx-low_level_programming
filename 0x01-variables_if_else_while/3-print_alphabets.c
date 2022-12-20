4768600c02d91480551857ee80edfc37c980860d
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-21 00:18:18 +0100

    print upper and lowercase letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point into the program
+ * @void: Does not accept any parameter
+ *
+ * Return: returns (0)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
