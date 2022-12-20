cc3506b7fed366e6761ded7e4e5b1f2b4531f3dc
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-20 05:47:27 +0100

    count string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _strlen - retuns the length of a string
+ * @str: the string to count its length
+ *
+ * Return: int length
+
+int _strlen(char *s)
+{
+	int i = 1;
+
+	while (str[i] != '\0')
+	{
+		i++
+	}
+
+	return (i)
+}
