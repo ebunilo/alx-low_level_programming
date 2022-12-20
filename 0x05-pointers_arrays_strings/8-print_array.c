e9644f0340f9d0c642a6a4fd500302bb375ad5d5
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-20 12:57:38 +0100

    alternate print statement

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,8 +12,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i <= n-1; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", a[i]);
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
 	}
 }
