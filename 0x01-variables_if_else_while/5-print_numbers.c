76588ea329bb3426fe7f05987c3720c2c3899cf0
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-21 00:46:03 +0100

    print all base 10 single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @void: does not take any parameter
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+}
