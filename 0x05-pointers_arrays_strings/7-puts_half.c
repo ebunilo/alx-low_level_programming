e5ef5aa987a6178d6d8bb8f495bc0cc4bfc2252b
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-20 10:50:04 +0100

    print half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include <stdin.io>
+
+/**
+ * puts_half - Print half of a string followed by a newline
+ * @str: string argument
+ *
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int i, len, end;
+
+	len = strlen(str) / 2;
+	end = 2 * len;
+
+	for (i = len; i < end; i++)
+		putchar(*(str + i));
+}
