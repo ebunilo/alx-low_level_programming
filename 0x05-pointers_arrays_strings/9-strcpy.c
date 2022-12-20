44c2921315abba22411a85ee799be31e2531cde7
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-20 13:50:19 +0100

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * _strcpy - Copy string including null from source to destination
+ * @dest: argument - destination address
+ * @src: argument - source address
+ *
+ * Return: pointer to destination
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++
+	}
+	*(dest + i+1) = '\0';
+
+	return (*dest);
+}
