680b6830b2430dc4178d82544be751179806ec74
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-21 00:03:58 +0100

    print lowercase letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to the program
+ * @void: does not accept any parameter
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = a; c <= z; c++)
+		putchar(c);
+	putchar('\n');
+}
