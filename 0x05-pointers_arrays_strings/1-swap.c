082a81d084c52e0a41084227317444c4a0e675ac
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-20 05:14:38 +0100

    Swap tow integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * swap_int - Swap two integers
+ * @*a *b: the two integers to swap
+ *
+ * Return: Nothing
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	a = *b;
+	*b = tmp;
+}
