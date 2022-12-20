690b1cdc01485ac171673339c6195d361407417d
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-20 04:52:40 +0100

    add files

---
+++ 0x05-pointers_array_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Reset value to 98
+ * @p: takes pointer to an int
+ *
+ * Return: Always (0)
+
+void reset_to_98(int *p)
+{
+	*p = 98;
+
+	return;
+}
---
+++ 0x05-pointers_array_strings/README.md
@@ -0,0 +1,1 @@
+Pointer. Array and Strings
