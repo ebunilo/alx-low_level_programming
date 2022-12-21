ce6a7726091074ab99f85730a8e8b30f2b930b1d
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-21 01:37:25 +0100

    print heaxadecials

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @void: does not take any paramenter
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '1'; i <= 'f'; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
