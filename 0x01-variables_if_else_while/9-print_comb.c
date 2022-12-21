8fb129874101c38968c41f96413ee0561c557332
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-21 02:15:26 +0100

    add newline character

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,16 +12,13 @@ int main(void)
 	int i;
 
 	for (i = '0'; i <= '9'; i++)
+		putchar(i);
 		if (i != '9')
 		{
-			putchar(i);
 			putchar(',');
 			putchar(' ');
 		}
-		else
-		{
-			putchar(i);
-		}
+		putchar('\n');
 
 	return (0);
 }
