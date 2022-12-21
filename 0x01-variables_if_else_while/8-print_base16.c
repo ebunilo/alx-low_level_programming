916a59bcf9158d4ada6c3ac690dda69fc2927e32
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-21 01:44:42 +0100

    concat digits and letters to form hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,12 @@
 
 int main(void)
 {
-	int i;
+	int i, c;
 
-	for (i = '1'; i <= 'f'; i++)
+	for (i = '1'; i <= '9'; i++)
 		putchar(i);
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
 	putchar('\n');
 
 	return (0);
