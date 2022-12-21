9d05a6f92b7133056ce7d61ab3823c2be363edc9
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-21 02:06:17 +0100

    add braces

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,10 +13,14 @@ int main(void)
 
 	for (i = '1'; i <= '9'; i++)
 		if (i != '9')
+		{
 			putchar(i);
-			putchar(", ");
+			putchar(', ');
+		}
 		else
+		{
 			putchar(i);
+		}
 	putchar('\n');
 
 	return (0);
