1f7ae3b87a4fbcfaf845e0e56fe97b64256eefd7
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-21 02:03:14 +0100

    print comma separated digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of single digit numbers, separated by coma
+ * @void: does not take any parameter
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '1'; i <= '9'; i++)
+		if (i != '9')
+			putchar(i);
+			putchar(", ");
+		else
+			putchar(i);
+	putchar('\n');
+
+	return (0);
+}
