6c337cfea9f085d420e080a3698eada5996f5bb7
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-21 02:09:05 +0100

    single character putchar

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,13 +15,13 @@ int main(void)
 		if (i != '9')
 		{
 			putchar(i);
-			putchar(', ');
+			putchar(',');
+			putchar(' ');
 		}
 		else
 		{
 			putchar(i);
 		}
-	putchar('\n');
 
 	return (0);
 }
