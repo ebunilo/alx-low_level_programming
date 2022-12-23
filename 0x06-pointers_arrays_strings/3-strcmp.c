3fd6634f703763241b7597ce46e77bb08eb1c6d7
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-23 04:42:21 +0100

    trailing whitespaces: Betty

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,7 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0, k = 0; s1_len = 0, s2_len = 0;
+	int i = 0, j = 0, k = 0, s1_len = 0, s2_len = 0;
 
 	while (s1[i++])
 		s1_len++;
@@ -23,10 +23,10 @@ int _strcmp(char *s1, char *s2)
 
 	if (s1_len > s2_len)
 		return (1);
-	
+
 	for (k = 0; k < s1_len; k++)
-		if (str1[k] != str2[k])
+		if (s1[k] != s2[k])
 			return (1);
 
-	return (0)
+	return (0);
 }
