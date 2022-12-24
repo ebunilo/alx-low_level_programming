8144526545182fb3620f5d040d257d4c70c30bbb
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-24 23:39:43 +0100

    print _putchar

---
+++ 0x02-functions-nested-loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point into the program that prints _putchar
+ * @void: Does not take any paramenter
+ *
+ * Return: Returns (0)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
