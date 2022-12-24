960b40ba989a56ade0e267edddb3d5e785e1ac60
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-24 23:50:21 +0100

    rename directory

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _atoi - Convert string to integer
+ * @s: parameter
+ *
+ * Return: integer or 0 if there are no numbers
+ */
+
+int _atoi(char *s)
+{
+	int i, valid = 1;
+
+	while (valid)
+	{
+		if (s[i] == "-" && s[i + 1] 
+	}
+}
