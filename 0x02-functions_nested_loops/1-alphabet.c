ba7960907011facc53d485f9d882238d298ac613
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-25 00:22:41 +0100

    print lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet
+ * @void: Does not take any paramenter
+ * 
+ * Return: Returns Nothing
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = "a"; c <= "z", c++)
+		_putchar(c);
+	_putchar('\n');
+}
