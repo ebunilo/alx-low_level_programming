9e775303f1f9395b28283f78a20b38aed7c87853
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-25 07:40:00 +0100

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+/**
+ * add - Add two integers
+ * @n1: first number paramenter
+ * @n2: second number parameter
+ *
+ * Return: Return the sum of n1 and n2
+ */
+
+int add(int n1, int n2)
+{
+	int add;
+
+	add = n1 + n2;
+	printf("%d\n", add);
+}
