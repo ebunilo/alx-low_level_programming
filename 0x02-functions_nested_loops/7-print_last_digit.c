badc4c28184ab56adf11d55a0de293ff60d5e993
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-25 04:08:03 +0100

    use printf

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * print_last_digit - Return the value of the last digit
@@ -10,6 +10,6 @@
 int print_last_digit(int j)
 {
 	j = j % 10;
-	_putchar(j);
+	printf("%d", j);
 	return (j);
 }
