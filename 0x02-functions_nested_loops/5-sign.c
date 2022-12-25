8a45e52c3e1df90cd952ba4fa423b2797d446b4f
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-25 01:57:39 +0100

    change _putchar to putchar

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		putchar('0');
 		return (0);
 	}
 	else
 	{
-		_putchar('-1');
+		putchar('-1');
 		return (-1);
 	}
 }
