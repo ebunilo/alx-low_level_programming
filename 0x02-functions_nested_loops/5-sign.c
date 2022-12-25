5c764b00f4a42fcaa980da7063cf4b89c376b4c2
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-25 01:55:51 +0100

    print the sign of a numbe

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_sign - print the sign of a numbe
+ * @n: the number to check sign
+ * 
+ * Return: Return 1 and print + if n is greater than 0, return 0 if 0 and -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-1');
+		return (-1);
+	}
+}
