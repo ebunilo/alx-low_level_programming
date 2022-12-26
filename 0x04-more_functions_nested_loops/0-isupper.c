fa47b3a3256b7ad8be505730f41986c854f89fd6
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-26 10:57:45 +0100

    refactor

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,14 @@
 /**
  * _isupper - return 1 if parameter is uppercase
  * @c: the parameter to check
+ * 
  * Return: 0 (Not success)
  */
 
 int _isupper(int c)
 {
-	int i;
-
-	for (i = 'A'; i <= 'Z'; i++)
-	{
-		if (i == c)
-			return (1);
-	}
-	return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
