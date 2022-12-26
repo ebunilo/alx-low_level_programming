3d00c5fd97b8fcefaf41861d6e1f50fed9cd13eb
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-26 21:46:56 +0100

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers 0 to 14, followed by newline
+ * @void: Does not take any parameter
+ *
+ * Return: Nothing to return
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 14; i++)
+	{
+		if (!(i / 10))
+		{
+			_putchar(i);
+		}
+		else
+		{
+			_putchar(i / 10);
+			_putchar(i % 10);
+		}
+	putchar('\n');
+	}
+}
