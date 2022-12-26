9aa440e5356e59de7be7f5efcd83ea25c51943fd
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-26 22:07:28 +0100

    use _putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,7 @@ void more_numbers(void)
 
 	for (i = 0; i <= 14; i++)
 	{
-		if (!(i / 10))
+		if (i / 10 == 0)
 		{
 			_putchar(i);
 		}
@@ -22,6 +22,6 @@ void more_numbers(void)
 			_putchar(i / 10);
 			_putchar(i % 10);
 		}
-	putchar('\n');
+	_putchar('\n');
 	}
 }
