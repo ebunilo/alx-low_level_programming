a753567a6805610af5ec4f434ddadc5e5ff61a5c
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-26 13:03:50 +0100

    print most numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_most_numbes - Print numbers 0 to 9, without 2 and 4
+ * @void: Does not take any parameter
+ *
+ * Return: Nothing to return
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i = 9; i++)
+	{
+		if (i != 2 && i != 4)
+			putchar(i);
+	}
+	putchar('\n')
+}
