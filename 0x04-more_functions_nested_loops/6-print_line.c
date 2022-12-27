471039d26d36b9cd28bc6c8380ceb50ed051b830
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-27 07:43:59 +0100

    print a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_line - Draw a straight line in the terminal 
+ * @n: lenght of line (numbe of characters of _ )
+ *
+ * Return: Nothing to return
+ */
+
+void print_line(int n);
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			putchar('_');
+	}
+	putchar('\n');
+}
+
