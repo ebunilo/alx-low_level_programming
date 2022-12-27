48c650fdbca384e61837d63747335d717dbd7c48
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-27 06:40:58 +0100

    remove + '0' from _putchar, update inequality in if-statement

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,9 +15,9 @@ void more_numbers(void)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			if (i > 10)
-				_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
+			if (i >= 10)
+				_putchar(i / 10);
+			_putchar(i % 10);
 		}
 		_putchar('\n');
 		j++;
