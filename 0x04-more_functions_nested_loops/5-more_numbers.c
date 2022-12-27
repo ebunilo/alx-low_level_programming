974b823ca0f617db101dfc1782b4adf1a3fb9d8e
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2022-12-27 06:42:38 +0100

    add + '0' to _putchar

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,8 +16,8 @@ void more_numbers(void)
 		for (i = 0; i <= 14; i++)
 		{
 			if (i >= 10)
-				_putchar(i / 10);
-			_putchar(i % 10);
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 		j++;
