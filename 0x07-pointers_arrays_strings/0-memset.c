cd46188a47178147c8a1344116d9b79a3f8b56db
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-01-16 19:21:18 +0100

    typedefs and structs

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /*
- * _memset - fills memory block with a constant byte
+ * _memset - Fills the first n bytes of the memory area with constant b
  * @s: memory block address
  * @b: char to be used
  * @n: int parameter
+ * Description: Fills the first n bytes of the memory area with bytes
+ *
  * Return: pointer to the memory block
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	while (n)
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - A struct for dog
+ * @name: Name of dog
+ * @owner: Name of owner
+ * @age: Age of dog
+*/
+
+struct dog
+{
+    char *name;
+    char *owner;
+    float age;
+};
