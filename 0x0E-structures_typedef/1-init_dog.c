b68976aba88b62f765e8f954464e2806dacacc8c
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-01-17 05:16:36 +0100

    remove malloc

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,20 +1,17 @@
 #include "main.h"
 #include "dog.h"
-#include <stdlib.h>
 
 /**
  * init_dog - Initialize a variable of type struct dog
- * @dog: Struct dog
+ * @d: Struct dog
  * @name: Name of dog
  * @age: Age of dog
  * @owner: Name of owner of dog
- *
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
-	d -> name = name;
-	d -> owner = owner;
-	d -> age = age;
+	d->name = name;
+	d->owner = owner;
+	d->age = age;
 }
