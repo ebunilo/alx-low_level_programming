ab155fa5d36e577537f11d747e4fd8ad573f948c
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-01-18 02:48:40 +0100

    free dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Name of owner of dog
+ *
+ * Return: new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+
+	return (new_dog);
+}
+
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * free_dog - Free dogs memory
+ * @d: dog struct to free
+ * 
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
+
