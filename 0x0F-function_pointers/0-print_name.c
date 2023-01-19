a56dac13cb735ecc5a9b162d4e365b525abbd318
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-01-19 06:36:32 +0100

    add files

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name
+ * @name: Name to print
+ * @f: Function that prints the name 
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	char name_;
+
+	name_ = *name;
+	f(name_);
+}
+
