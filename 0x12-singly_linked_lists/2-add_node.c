f779e267af3cf14d2da05025d7e571916fffdc28
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-01-29 23:42:53 +0100

    add new node at beginning of a linked-list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,46 @@
+#include <string.h>
+#include "lists.h"
+
+/**
+ * _strlen - Return the length of a string
+ * @s: the string to count its length
+ *
+ * Return: int length of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+
+	return (i);
+}
+
+/**
+ * add_node - Add a new node at the beggining of a list
+ * @head: Pointer to the beginning of the list
+ * @str: String stored in a node
+ *
+ * Return: address of the new node or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = *head;
+	*head = new;
+
+	free(new->str);
+	free(new);
+
+	return (new);
+}
