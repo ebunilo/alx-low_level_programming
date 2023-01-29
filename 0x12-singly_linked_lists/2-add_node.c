b4554eaa0d11632076f6aaa9529b2579bb1c981c
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-01-30 00:01:22 +0100

    match parameter type

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
@@ -8,11 +9,11 @@
  * Return: int length of string
  */
 
-int _strlen(char *s)
+int _strlen(const char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (*(s + i) != '\0')
 		i++;
 
 	return (i);
