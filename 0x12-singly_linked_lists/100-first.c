ba6b055af9addbbb3e554e10000ebafb85da9a0b
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-01-30 02:42:32 +0100

    print function first before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * print_first - Apply the constructor attribute
+ * so it will be printed before main
+ */
+
+void print_first(void) __attribute__ ((constructor));
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
