112aa8b3d1364ab9c6b92cba99e49c35a4233cef
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-01-30 02:46:36 +0100

    add documentation - Betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,10 +3,14 @@
 /**
  * print_first - Apply the constructor attribute
  * so it will be printed before main
+ *
  */
-
 void print_first(void) __attribute__ ((constructor));
 
+/**
+ * print_first - Implementation
+ *
+ */
 void print_first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
