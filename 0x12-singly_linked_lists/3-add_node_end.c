14ab0307da6b9ad9595e1261a489dcb231dd52d7
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-01-30 01:44:35 +0100

    convert spaces to tab

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -32,7 +32,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new, *temp;
 
 	if (str == NULL)
-                return (NULL);
+		return (NULL);
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
