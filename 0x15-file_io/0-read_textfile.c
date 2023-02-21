d662446d29a5b0f24ff2bb0e0124a7e62957a30c
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-02-21 03:47:11 +0100

    add newline at end of file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,3 @@
-#include <fcntl.h>
 #include <stdlib.h>
 #include "main.h"
 
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,3 +10,4 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif /* MAIN_H */
+
