c70182df79b4b6339c664bee196ef22de7013e3e
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-02-21 04:14:38 +0100

    create file function

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ * @filename: Name of the file to be created
+ * @text_content: A NULL terminated string to be added to the file
+ * Description: Created file must have these permissions rw-------
+ * if the file already exists, do not change the permission
+ * if the file already exists, truncate it
+ * if text_content is NULL, create an empty file
+ * if filename is NULL return -1
+ *
+ * Return: Returns 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	while (text_content)
+		len++;
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	return (0);
+}
+
