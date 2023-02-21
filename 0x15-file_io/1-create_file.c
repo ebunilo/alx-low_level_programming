c8a29ed113ff09fe09d89af90b533ebb6879b715
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-02-21 04:26:53 +0100

    iterate with for loop

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,8 +20,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content)
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
 		len++;
+	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
